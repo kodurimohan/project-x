d3735cfd7567f568b87e8893dbe7ee7d5d1ed02c
kodurimohan <kodurimohan@outlook.com>
2023-11-13 16:32:16 +0530

    completed Settings Page functionalities

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -11,6 +11,11 @@ export function Mainnav({
   const params = useParams();
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
